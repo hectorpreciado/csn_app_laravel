4db4f6bd720e8696ca8f37459c91f64453684951
hectorpreciado <99374559+hectorpreciado@users.noreply.github.com>
2023-11-28 20:54:08 -0600

    Final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
             <header>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                     <div class="container-fluid">
-                      <a class="navbar-brand" href="#">Navbar</a>
+                      <a class="navbar-brand" href="#">Cátalogo</a>
                       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
@@ -34,14 +34,11 @@
                               Dropdown
                             </a>
                             <ul class="dropdown-menu">
-                              <li><a class="dropdown-item" href="#">Action</a></li>
-                              <li><a class="dropdown-item" href="#">Another action</a></li>
+                              <li><a class="dropdown-item" href="#">Agregar</a></li>
+                              <li><a class="dropdown-item" href="#">Editar</a></li>
                               <li><hr class="dropdown-divider"></li>
-                              <li><a class="dropdown-item" href="#">Something else here</a></li>
+                              <li><a class="dropdown-item" href="#">Eliminar</a></li>
                             </ul>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                           </li>
                         </ul>
                         <form class="d-flex" role="search">
